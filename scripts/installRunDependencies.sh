f3c9dde9d4f4b5bed98a6b79874c14e56d97d06b
Ivan Moreno <ivan1697@gmail.com>
2019-11-26 22:27:05 -0600

    add set -e in script

--- scripts/installRunDependencies.sh
+++ scripts/installRunDependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 #install packages to run 
 apt-get update &&
 apt-get install -y \
@@ -14,6 +16,6 @@ apt-get clean
 echo 'Delete /var/lib/apt/lists/* /tmp/* /var/tmp/*' 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-mysql -u root -e "CREATE USER ‘lorasender’@’localhost’ IDENTIFIED BY ‘lora-p@s2’;"
-mysql -u root -e "GRANT SELECT ON EMCA.* to ‘lorasender’@’localhost’;"
+mysql -u root -e "CREATE USER 'lorasender'@'localhost' IDENTIFIED BY 'lora-p@s2';"
+mysql -u root -e "GRANT SELECT ON EMCA.* to 'lorasender'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
